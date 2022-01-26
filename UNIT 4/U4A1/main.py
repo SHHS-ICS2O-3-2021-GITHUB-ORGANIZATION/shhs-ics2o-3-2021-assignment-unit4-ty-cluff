70e04d32187088c30ebc5aa78491e9425a17dfe7
ty_cluff <97173924+ty-cluff@users.noreply.github.com>
2022-01-26 14:02:48 +0000

    Q1 Volume Folder

--- UNIT 4/U4A1/main.py
+++ UNIT 4/U4A1/main.py
@@ -1,25 +1,34 @@
-# NAME OF AUTHOR:  
-# NAME OF THE PROGRAM:  
-# DATE OF CREATION:  
-# PURPOSE OF PROGRAM:  
+# Ty
+# NAME OF THE PROGRAM: Q1 Volume
+# DATE OF CREATION: Jan 26 
+# PURPOSE OF PROGRAM: To give me the volume of a prisim 
 
 
 # VARIABLE DEFINITION
+height = 0
+width = 0
+depth = 0
 
-
-
-
+sum_of_items = 0
+total = 0
 
 # INPUT
-
+print("Please Enter Height")
+height = int(input())
+print("Please Enter Width")
+width = int(input())
+print("Please Enter Depth")
+depth = int(input())
 
 
 
 
 # PROCESSING
-
+sum_of_items = height * width * depth
 
 
 
 
 # OUTPUT
+#volume displayed
+print("\nthe volume of the prisim is: " + str(sum_of_items))
