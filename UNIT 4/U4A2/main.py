def18a97966bebf65b788189c2207a686e85da6a
ty-cluff <97173924+ty-cluff@users.noreply.github.com>
2022-01-26 10:23:19 -0500

    Update main.py

--- UNIT 4/U4A2/main.py
+++ UNIT 4/U4A2/main.py
@@ -20,6 +20,59 @@
 
 
 
+ NAME OF AUTHOR:  Ty
+# NAME OF THE PROGRAM:  tax calculator
+# DATE OF CREATION:  Jan 26
+# PURPOSE OF PROGRAM:  tax
 
+
+# VARIABLE DEFINITION = 0
+item1 = 0
+iten2 = 0
+item3 = 0
+item4 = 0
+item5 = 0
+item6 = 0
+item7 = 0
+item8 = 0
+item9 = 0
+item10 = 0
+
+
+# INPUT
+print("Enter price of first item")
+item1 = int(input())
+print("Enter price of second item")
+item2 = int(input())
+print("Enter price of third item")
+item3 = int(input())
+print("Enter price of fourth item")
+item4 = int(input())
+print("Enter price of fifth item")
+item5 = int(input())
+print("Enter price of sixth item")
+item6 = int(input())
+print("Enter price of seventh item")
+item7 = int(input())
+print("Enter price of eighth item")
+item8 = int(input())
+print("Enter price of ninth item")
+item9 = int(input())
+print("Enter price of tenth item")
+item10 = int(input())
+
+
+
+
+
+
+# PROCESSING
+sum_of_items = item1 + item2 + item3 + item4 + item5 + item6 + item7 + item8 + item9 + item10
+
+
+
+
+# OUTPUT
+print("\nThe Total Is
 
 # OUTPUT
